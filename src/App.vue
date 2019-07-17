a5244ab53f1b5ca1c06982bda8f33bcb0bba04ce
cgomesj <32676188+cgomesj@users.noreply.github.com>
2019-07-16 21:27:55 -0300

    vue-directives-v0.1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,115 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Built-in Directives</h1>
+        <p v-text="'Señor Bob'"></p>
+        <p v-html="'<strong>Señor Bob</strong>'"></p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p v-highlight.delayed="'gray'">This is nice with color</p>
+        <p
+          v-local-highlight.delayed.blink="localDirectiveProps"
+        >This is a local directive for blinking colors</p>
+      </div>
+    </div>
+
+    <hr />
+
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Directives Exercise</h1>
+        <button v-my-on:click="clicked" class="btn btn-primary">v-my-on custom directive</button>
+      </div>
+
+      <div class="w-100 my-2"></div>
+
+      <div
+        class="ml-3"
+        style="width: 100px; height: 100px; background-color: lightgreen"
+        v-my-on:mouseenter="mouseEnter"
+        v-my-on:mouseleave="mouseLeave"
+      ></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { setTimeout, setInterval } from "timers";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  directives: {
+    "local-highlight": {
+      bind(el, binding, vnode) {
+        var delay = 0;
+
+        if (binding.modifiers["delayed"]) {
+          delay = 1000;
+        }
+
+        if (binding.modifiers["blink"]) {
+          let mainColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
+          let currentColor = mainColor;
+
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == secondColor
+                ? (currentColor = mainColor)
+                : (currentColor = secondColor);
+
+              el.style.color = currentColor;
+            }, binding.value.blinkDelay);
+          }, delay);
+        } else {
+          setTimeout(() => {
+            el.style.color = binding.value.mainColor;
+          }, delay);
+        }
+      }
+    },
+
+    "my-on": {
+      bind(el, binding) {
+        // el.onclick = function() {
+        // binding.value();
+        // };
+
+        const type = binding.arg;
+        const fn = binding.value;
+        el.addEventListener(type, fn);
+      }
+    }
+  },
+
+  data() {
+    return {
+      localDirectiveProps: {
+        mainColor: "red",
+        secondColor: "green",
+        blinkDelay: 300
+      }
+    };
+  },
+
+  methods: {
+    clicked() {
+      alert("Button was clicked!");
+    },
+
+    mouseEnter() {
+      console.log("Mouse entered");
+    },
+
+    mouseLeave() {
+      console.log("Mouse left");
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.directive("highlight", {
+  bind(el, binding, vnode) {
+    // el.style.color = "green";
+    // el.style.color = binding.value;
+
+    var delay = 0;
+
+    if (binding.modifiers["delayed"]) {
+      delay = 1000;
+    }
+
+    setTimeout(() => {
+      if (binding.arg == "background") {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
+  }
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
